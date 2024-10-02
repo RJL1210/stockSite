b5b7568c029f9fddec2f38907069aad0c0c9f3be
RJL1210 <144273649+RJL1210@users.noreply.github.com>
2024-10-01 23:44:39 -0700

    set up basic api call and parsing

--- stocks/graphs/views.py
+++ stocks/graphs/views.py
@@ -14,11 +14,23 @@ def home(request):
 
     try:
         api = json.loads(url.content)
+        processed_data = { 
+            'symbol': api['Global Quote']['01. symbol'],
+            'open': api['Global Quote']['02. open'],
+            'high': api['Global Quote']['03. high'],
+            'low': api['Global Quote']['04. low'],
+            'price': api['Global Quote']['05. price'],
+            'volume': api['Global Quote']['06. volume'],
+            'latest trading day': api['Global Quote']['07. latest trading day'],
+            'previous close': api['Global Quote']['08. previous close'],
+            'change': api['Global Quote']['09. change'],
+            'change percent': api['Global Quote']['10. change percent'],
+        }
     except Exception as e:
         api = "Error..."
 
 
-    return render(request, 'home.html', {'api': api,})
+    return render(request, 'home.html', {'api': api, 'processed_api': processed_data})
 
 def about(request):
     return render(request, 'about.html', {})
