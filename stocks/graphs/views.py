325e7a120fff397d4926b8ac5ab668819a804f5c
RJL1210 <144273649+RJL1210@users.noreply.github.com>
2024-10-02 19:27:39 -0700

    added table and graph to home page, add stock table is still broken?

--- stocks/graphs/views.py
+++ stocks/graphs/views.py
@@ -4,14 +4,30 @@ from django.contrib import messages
 from .models import Stock
 from .forms import StockForm
 
+context = {
+        "api_key": settings.ALPHA_VANTAGE_API,
+}
+
+def readNumber(value):
+    try:
+        value = float(value)
+    except (TypeError, ValueError):
+        pass    
+
+    if value >= 1_000_000_000_000:
+        return f'{value / 1_000_000_000_000:.2f}T'
+    elif value >= 1_000_000_000:
+        return f'{value / 1_000_000_000:.2f}B'
+    elif value >= 1_000_000:
+        return f'{value / 1_000_000:.2f}M'
+    elif value >= 1_000:
+        return f'{value / 1_000:.2f}K'
+    else:
+        return str(value)
+
 def home(request):
     import requests
     import json
-
-    context = {
-        "api_key": settings.ALPHA_VANTAGE_API,
-    }
-    
 
     if request.method == 'POST':
         ticker = request.POST['ticker']
@@ -49,7 +65,7 @@ def home(request):
             processed_api['change'] = api['Global Quote']['09. change']
             processed_api['change percent'] = api['Global Quote']['10. change percent']
 
-            processed_api['marketcap'] = overview['MarketCapitalization']
+            processed_api['marketcap'] = readNumber(overview['MarketCapitalization'])
             processed_api['companyname'] = overview['Name']
             processed_api['52weekhigh'] = overview['52WeekHigh']
             processed_api['52weeklow'] = overview['52WeekLow']
@@ -69,6 +85,8 @@ def about(request):
     return render(request, 'about.html', {})
 
 def add_stock(request):
+    import requests
+    import json
 
     if request.method == 'POST':
         form = StockForm(request.POST or None)
@@ -79,10 +97,51 @@ def add_stock(request):
             return redirect('add_stock')
     else:
 
-    
+        processed_apis = {}
+        index = 0
         ticker = Stock.objects.all()
+        curr_api = {}
 
-        return render(request, 'add_stock.html', {'ticker': ticker})
+        for symb in ticker:
+            api_request = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + str(symb) + '&apikey=' + context['api_key'])
+            overview_url = requests.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol=' + str(symb) + '&apikey=' + context['api_key'])
+
+            try: 
+                api = json.loads(api_request.content)
+                overview = json.loads(overview_url.content)
+                if api['Global Quote']:
+                    curr_api['outOfCalls'] = False
+            except Exception as e:
+                default_url = requests.get('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo')
+                overview_default = requests.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo')
+                api = json.loads(default_url.content)
+                overview = json.loads(overview_default.content)
+                curr_api['outOfCalls'] = True
+
+            print(api)
+
+            curr_api['symbol'] = api['Global Quote']['01. symbol']
+            curr_api['open'] = api['Global Quote']['02. open']
+            curr_api['high'] = api['Global Quote']['03. high']
+            curr_api['low'] = api['Global Quote']['04. low']
+            curr_api['price'] = api['Global Quote']['05. price']
+            curr_api['volume'] = api['Global Quote']['06. volume']
+            curr_api['latest trading day'] = api['Global Quote']['07. latest trading day']
+            curr_api['previous close'] = api['Global Quote']['08. previous close']
+            curr_api['change'] = api['Global Quote']['09. change']
+            curr_api['change percent'] = api['Global Quote']['10. change percent']
+
+            curr_api['marketcap'] = readNumber(overview['MarketCapitalization'])
+            curr_api['companyname'] = overview['Name']
+            curr_api['52weekhigh'] = overview['52WeekHigh']
+            curr_api['52weeklow'] = overview['52WeekLow']
+            
+            processed_apis[index] = curr_api
+            index += 1
+            
+            combined = zip(ticker, processed_apis)
+
+        return render(request, 'add_stock.html', {'ticker': ticker, 'combined': combined})
     
 def delete_stock(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
